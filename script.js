bfa613a4dc9a72ecc21ec06b157b02f9f8c87d17
unknown <mhlonipheninto77@gmail.com>
2021-11-08 16:39:39 +0200

    name added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const   names = ["Kagisho", "Mhlonipheni", "Ndunduzu", "Sphoie", "Selina", "Percious", "Andisiwe","Vumuzi", "Wandile", "Khumbulani", "Lucky", "Sithembiso", "Nkosphendule", "Happy", "Mmpaseka"]
+
+const display = document.getElementById("display");
+
+for (let i = 0; i < names.length; i++) {
+   display.innerHTML += "<li>"  +   names [i]
+  }
+
+  
