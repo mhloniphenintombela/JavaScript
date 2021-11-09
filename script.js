8b1f8b2a32a69b21ec4aeb1098455bf08febe8e5
unknown <mhlonipheninto77@gmail.com>
2021-11-09 13:59:25 +0200

    this page says

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-
-const   names = ["Kagisho", "Mhlonipheni", "Ndunduzu", "Sphoie", "Selina", "Percious", "Andisiwe","Vumuzi", "Wandile", "Khumbulani", "Lucky", "Sithembiso", "Nkosphendule", "Happy", "Mmpaseka"]
-
-const display = document.getElementById("display");
-
-for (let i = 0; i < names.length; i++) {
-   display.innerHTML += "<li>"  +   names [i]
-  }
-
+src="file.js">
+alert(1); // the content is ignored, because src is set
   
