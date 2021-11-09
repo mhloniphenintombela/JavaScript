e477e714a38f8c7e792769539aae3118a55c7ba3
unknown <mhlonipheninto77@gmail.com>
2021-11-09 14:32:15 +0200

    3

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 
-let hello = 'Hello world!';
-let message;
-// copy 'Hello world' from hello into message
-message = hello;
-// now two variables hold the same data
-alert(hello); // Hello world!
-alert(message); // Hello world!
+let $ = 1; // declared a variable with the name "$"
+let _ = 2; // and now a variable with the name "_"
+alert($ + _); // 3
