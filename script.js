d5f06878a86fc2ce612beaf1770dccfeb1d9ab12
unknown <mhlonipheninto77@gmail.com>
2021-11-09 15:22:47 +0200

    isGreater

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-let nameFieldChecked = true; // yes, name field is checked
-let ageFieldChecked = false; // no, age field is not checked
+
+let isGreater = 4 > 1;
+alert( isGreater ); // true (the comparison result is "yes")
