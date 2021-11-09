84a9162fc6cdc74e0cdf673a1a9f2d9cd6a52098
unknown <mhlonipheninto77@gmail.com>
2021-11-09 16:36:18 +0200

    5

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 
- // No effect on numbers
-let x = 1;
-alert( +x ); // 1
-let y = -2;
-alert( +y ); // -2
-// Converts non-numbers
-alert( +true ); // 1
-alert( +"" ); // 0
+ let apples = "2";
+ let oranges = "3";
+ alert( apples + oranges ); // "23", the binary plus concatenates strings
  
  
