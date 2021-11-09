a43ce7fcc46d9383999393553713149753b0358c
unknown <mhlonipheninto77@gmail.com>
2021-11-09 15:02:26 +0200

    Hello, John!

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert( "not a number" / 2 ); // NaN, such division is erroneous
+let name = "John";
+// embed a variable
+alert( `Hello, ${name}!` ); // Hello, John!
+// embed an expression
+alert( `the result is ${1 + 2}` ); // the result is 3
