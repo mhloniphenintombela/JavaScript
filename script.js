0368ddd6dc8afded56632708b704d0fc0c431d98
unknown <mhlonipheninto77@gmail.com>
2021-11-09 15:10:31 +0200

    the result is

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-let name = "John";
-// embed a variable
-alert( `Hello, ${name}!` ); // Hello, John!
-// embed an expression
-alert( `the result is ${1 + 2}` ); // the result is 3
+alert( "the result is ${1 + 2}" ); // the result is ${1 + 2} (double quotes do nothing)
