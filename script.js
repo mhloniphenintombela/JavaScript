52d4263dda06a04412a02f5194d47b3b5ccfa116
unknown <mhlonipheninto77@gmail.com>
2021-11-09 14:30:51 +0200

    Hello World

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
+let hello = 'Hello world!';
 let message;
-message = 'Hello!';
-message = 'World!'; // value changed
-alert(message);
+// copy 'Hello world' from hello into message
+message = hello;
+// now two variables hold the same data
+alert(hello); // Hello world!
+alert(message); // Hello world!
