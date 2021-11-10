1a8aa09a261b0bb10885b8b56157785bf434bfcf
unknown <mhlonipheninto77@gmail.com>
2021-11-10 08:30:52 +0200

    alert( 5 % 2 ); // 1 is a remainder of 5 divided by 2

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
- let a = 1;
- let b = 2;
- let c = 3 - (a = b + 1);
- alert( a ); // 3
- alert( c ); // 0
+ alert( 5 % 2 ); // 1 is a remainder of 5 divided by 2
+ alert( 8 % 3 ); // 2 is a remainder of 8 divided by 3
+ alert( 6 % 3 ); // 0 is a remainder of 6 divided by 3
