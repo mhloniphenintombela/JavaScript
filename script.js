1a702c4ec70b66520e70aa1a88a30a62e7e8fc3a
unknown <mhlonipheninto77@gmail.com>
2021-11-10 08:40:08 +0200

    let counter = 2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 
-alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root, that's maths)
-alert( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
+let counter = 2;
+counter++; // works the same as counter = counter + 1, but is shorter
+alert( counter ); // 3
