a054aa94a738dde350e7678ce021fca2dc14e210
unknown <mhlonipheninto77@gmail.com>
2021-11-10 08:35:14 +0200

    alert( 2 ** 2 ); // 4 (2 * 2)

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
- alert( 5 % 2 ); // 1 is a remainder of 5 divided by 2
- alert( 8 % 3 ); // 2 is a remainder of 8 divided by 3
- alert( 6 % 3 ); // 0 is a remainder of 6 divided by 3
+alert( 2 ** 2 ); // 4 (2 * 2)
+alert( 2 ** 3 ); // 8 (2 * 2 * 2)
+alert( 2 ** 4 ); // 16 (2 * 2 * 2 * 2)
