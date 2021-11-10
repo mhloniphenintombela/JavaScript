6e5b49da4ad357ced8da1614bc86f09004c4dd33
unknown <mhlonipheninto77@gmail.com>
2021-11-10 08:37:45 +0200

    alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root, that's maths)

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 
-alert( 2 ** 2 ); // 4 (2 * 2)
-alert( 2 ** 3 ); // 8 (2 * 2 * 2)
-alert( 2 ** 4 ); // 16 (2 * 2 * 2 * 2)
+alert( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root, that's maths)
+alert( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
