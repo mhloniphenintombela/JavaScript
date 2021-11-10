a76eee171efa345df2359320346c4179c5e7727b
unknown <mhlonipheninto77@gmail.com>
2021-11-10 08:43:17 +0200

    counter--; // works the same as counter = counter - 1, but is shorter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
 let counter = 2;
-counter++; // works the same as counter = counter + 1, but is shorter
-alert( counter ); // 3
+counter--; // works the same as counter = counter - 1, but is shorter
+alert( counter ); // 1
